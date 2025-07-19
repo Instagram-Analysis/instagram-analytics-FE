7740c32153d656c210dd05ddb84f2d1defd874a8
Qabin <qabinkicks@gmail.com>
2025-07-19 19:30:35 +0900

    Need to implement login

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "659d2637f5cb511c"; }
+/******/ 		__webpack_require__.h = function() { return "64a64a397c15a070"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- types/instagram.d.ts
+++ types/instagram.d.ts
@@ -1,13 +1,9 @@
 export interface Follower {
-  /** Unique ID of the user */
-  id: string
-  /** Instagram handle (without @) */
-  username: string
-  /** URL to their avatar image */
-  avatarUrl: string
+  id: string;
+  username: string;
+  avatarUrl: string;
 }
 
 export interface Liker extends Follower {
-  /** Number of likes they've given you */
-  likes: number
+  likes: number;
 }
